ca00fd32d087b780d9cffd85e2ab49b3bbc6ef37
hyunjin kim <hjazure19@gmail.com>
2023-12-07 23:22:16 +0900

    class Food

--- test.java
+++ test.java
@@ -1,4 +1,12 @@
-
+class Food{
+	int price;
+	int bonusPoint;
+	
+	Food(int price){
+		this.price=price;
+		bonusPoint=(int)(price/10);
+	}
+}
 public class test {
 
 	public static void main(String[] args) {
